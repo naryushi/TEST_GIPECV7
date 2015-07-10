72c5cd048046298ceba4b9b53447bddbe87eadf7
gregory_from_gipec <lefer.gregory@gmail.com>
2015-07-10 10:05:28 +0200

    a

--- projets/generate_project_v7.bat
+++ projets/generate_project_v7.bat
@@ -6,41 +6,37 @@ rem 			Création du projet
 rem =========================================
 @echo off
 set "project_name=%WORKSPACE%\projets\projet_jenkins.dhpt"
-@echo %project_name% 
->"%project_name%" (
-	@echo xwin-projet        2.0
-	@echo [general] 
-	@echo nom="jenkins"
-	@echo progexec="divalto.dhop"
-	@echo date="20150609160215070899"
-	@echo util="ROOT"
-	@echo [profildefaut]
-	@echo [profil]
-	@echo nom="jenkins"
-	@echo repobjet="%WORKSPACE%\objets"
-	@echo implicites="%JOB_NAME%jk.txt"
-	@echo [sousprojets]
-	@echo fic="jenkins.dhps"," "
-	@echo [projetsfusion]
-	@echo [fabricationmere]
-	@echo [autres]
-)
+>"%project_name%" @echo xwin-projet        2.0
+>>"%project_name%" @echo [general] 
+>>"%project_name%" @echo nom="jenkins"
+>>"%project_name%" @echo progexec="divalto.dhop"
+>>"%project_name%" @echo date="20150609160215070899"
+>>"%project_name%" @echo util="ROOT"
+>>"%project_name%" @echo [profildefaut]
+>>"%project_name%" @echo [profil]
+>>"%project_name%" @echo nom="jenkins"
+>>"%project_name%" @echo repobjet="%WORKSPACE%\objets"
+>>"%project_name%" @echo implicites="%JOB_NAME%jk.txt"
+>>"%project_name%" @echo [sousprojets]
+>>"%project_name%" @echo fic="jenkins.dhps"," "
+>>"%project_name%" @echo [projetsfusion]
+>>"%project_name%" @echo [fabricationmere]
+>>"%project_name%" @echo [autres]
+
 
 rem =========================================
 rem 		Création du sous-projet
 rem =========================================
 set "subproject_name=%WORKSPACE%\projets\jenkins.dhps"
->"%subproject_name%"(
-	@echo xwin-sprojet       2.0
-	@echo [general]
-	@echo date="20150702183409311499"
-	@echo util="ROOT"
-	@echo typetransport=3
-	@echo [communs]
-	@echo [fichiers]	
-	rem Ajout des fichiers dans le projet (sources std gipec + sources projet)
-	for %%f in (..\sources\*.dhsp) do echo %%f
-	for %%f in (..\sources_std_gipec\*.dhsp) do echo %%f
-	@echo [autres]
-)
+>"%subproject_name%" @echo xwin-sprojet       2.0
+>>"%subproject_name%" @echo [general]
+>>"%subproject_name%" @echo date="20150702183409311499"
+>>"%subproject_name%" @echo util="ROOT"
+>>"%subproject_name%" @echo typetransport=3
+>>"%subproject_name%" @echo [communs]
+>>"%subproject_name%" @echo [fichiers]>"%subproject_name%"
+rem Ajout des fichiers dans le projet (sources std gipec + sources projet)
+>>"%subproject_name%" for %%f in (..\sources\*.dhsp) do echo %%f
+>>"%subproject_name%" for %%f in (..\sources_std_gipec\*.dhsp) do echo %%f
+>>"%subproject_name%" @echo [autres]
 
